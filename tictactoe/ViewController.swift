7f3860800946b2dec84705f47bab09f1ab21361a
JGagacheva <j.gagaceva@gmail.com>
2023-01-29 11:25:22 -0800

    Dialog window works

--- tictactoe/ViewController.swift
+++ tictactoe/ViewController.swift
@@ -38,6 +38,41 @@ class ViewController: NSViewController {
     }
     
     
+    func winDialog(closing_text: String) -> Bool {
+        let alert = NSAlert()
+        alert.messageText = closing_text
+        alert.alertStyle = .warning
+        alert.addButton(withTitle: "Play Again")
+        alert.addButton(withTitle: "Exit")
+        alert.icon = nil
+        return alert.runModal() == .alertFirstButtonReturn
+    }
+    
+    func closeApp() {
+        NSApplication.shared.terminate(self)
+    }
+    
+   
+    func didWin(who: String) -> Bool {
+       for i in [0, 3, 6] {
+           if buttons[i + 0].title == who && buttons[i + 1].title == who && buttons[i + 2].title == who {
+               return true
+           }
+       }
+       for i in [0, 1, 2] {
+           if buttons[i + 0].title == who && buttons[i + 3].title == who && buttons[i + 6].title == who {
+               return true
+           }
+       }
+       if buttons[0].title == who && buttons[4].title == who && buttons[8].title == who {
+           return true
+       }
+       if buttons[2].title == who && buttons[4].title == who && buttons[6].title == who {
+           return true
+       }
+       return false
+   }
+    
     /// This function runs when a button is clicked on by the user
     /// - Parameter button: A reference to the button (i.e. button1, button2 etc) that the user clicked on
     @IBAction func onButtonClick(_ button: NSButton) {
@@ -52,39 +87,29 @@ class ViewController: NSViewController {
             }
         }
         
-        func winDialog(closing_text: String) -> Bool {
-            let alert = NSAlert()
-            alert.messageText = closing_text
-            alert.alertStyle = .warning
-            alert.addButton(withTitle: "Exit")
-            alert.addButton(withTitle: "Play Again")
-            return alert.runModal() == .alertFirstButtonReturn
-        }
-        
-        func didWin(who: String) -> Bool {
-            for i in [0, 3, 6] {
-                if buttons[i + 0].title == who && buttons[i + 1].title == who && buttons[i + 2].title == who {
-                    return true
+        func playAgain() {
+            for button in buttons {
+                if button.title == "X" || button.title == "O" {
+                    button.title = ""
+                    button.isEnabled = true
                 }
             }
-            for i in [0, 1, 2] {
-                if buttons[i + 0].title == who && buttons[i + 3].title == who && buttons[i + 6].title == who {
-                    return true
-                }
-            }
-            if buttons[0].title == who && buttons[4].title == who && buttons[8].title == who {
-                return true
-            }
-            if buttons[2].title == who && buttons[4].title == who && buttons[6].title == who {
-                return true
-            }
-            return false
         }
         
         if didWin(who: "X") {
-            _ = winDialog(closing_text: "You won!")
+            let shouldPlayAgain = winDialog(closing_text: "You won!")
+            if shouldPlayAgain == false {
+                closeApp()
+            } else {
+                playAgain()
+            }
         } else if didWin(who: "O") {
-            _ = winDialog(closing_text: "You lost.")
+            let shouldPlayAgain = winDialog(closing_text: "You lost!")
+            if shouldPlayAgain == false {
+                closeApp()
+            } else {
+                playAgain()
+            }
         }
         
     }
